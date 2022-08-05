bc2011e76a34baab0b4f44af01cc6a4cd9278f61
Sarina Chang <sarinachang7@gmail.com>
2022-08-05 01:54:11 -0400

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'boo', id: 0 },
+    { name: 'yoshi', id: 1 },
+    { name: 'mario', id: 2 },
+    { name: 'luigi', id: 3 },
+    { name: 'peach', id: 4 },
+    { name: 'bowser', id: 5 },
+  ]);
+
+  const pressHandler = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+    console.log(id);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <FlatList // lazy loads data, only shows what's on screen, better than map if a lot of data
+        numColumns={2} // split into columns
+        keyExtractor={(item) => item.id} // gets the id prop from the data
+        data={people} // data from state
+        renderItem={(
+          { item } // render text out
+        ) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -14,7 +46,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  item: {
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   },
 });
